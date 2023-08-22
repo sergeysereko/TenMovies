ddb67b8e1445005fd90caa68899c19f63828e9e0
sergey <sergeysereko@gmail.com>
2023-08-22 14:35:23 +0300

    #task 2 Добавил Контекстный класс MovieContext
    Для добавления данных в таблицу и инжектирования объекта Movie в методы контроллера

---
+++ TenMovies/Models/MovieContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TenMovies.Models
+{
+    public class MovieContext: DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public MovieContext(DbContextOptions<MovieContext> options)
+           : base(options)
+        {
+            if (Database.EnsureCreated())
+            {
+                Movies.Add(new Movie { Name = "Киндзадза", Director = "Трататат", Genre = "nfhffhfh", Year = 1986, Poster="nhfbhfdbj", Description="bdsgfuyejgv" });    
+                SaveChanges();
+            }
+        }
+    }
+}
+
+
+
