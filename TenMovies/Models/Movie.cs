f64914a2a9c1d25d0f6e5f32cf7cb3c526d85876
sergey <sergeysereko@gmail.com>
2023-08-22 13:39:12 +0300

    #task 1 Добавил класс Movie
    Необходим для создания таблицы в базе данных и хранения информации о фильме

--- TenMovies/Models/Movie.cs
+++ TenMovies/Models/Movie.cs
@@ -2,6 +2,12 @@
 {
     public class Movie
     {
+        public string Name { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public string Poster { get; set; }
+        public string Description { get; set; }
 
     }
 }
